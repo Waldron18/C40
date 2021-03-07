0285dbbfceaf6194986d9e1e559e360187b04ace
unknown <waldron>
2021-03-07 17:47:12 +0530

    c34

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 var ball;
+var db, position;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    db = firebase.database();
+
+    var ballpositionRef = db.ref("ball/position");
+    ballpositionRef.on("value", readPosition, showError);
 }
 
 function draw(){
@@ -26,4 +32,19 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+    db.ref("ball/position").set({
+        x: ball.x,
+        y: ball.y
+    });
+}
+
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("There is an error");
 }
