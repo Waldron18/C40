40f6b93e29f93e60f68e4d57f7aa025986f85bdf
unknown <waldron>
2021-03-09 19:13:27 +0530

    C35

--- js/form.js
+++ js/form.js
@@ -10,5 +10,16 @@ class Form{
         input.position(150,100);
         var button = createButton("PLAY");
         button.position(150,200);
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name = input.value();
+            playerCount = playerCount+1;
+            player.updateCount(playerCount);
+            player.update(name);
+            var greeting = createElement("h2","Welcome! "+name);
+            greeting.position(150,150);
+        });
+
     }
 }
