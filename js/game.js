a1c3feb91f0618dc1e6ca9d52f7597c45eea2643
unknown <waldron>
2021-04-16 15:56:25 +0530

    c37

--- js/game.js
+++ js/game.js
@@ -24,9 +24,13 @@ class Game {
             form.display();
         }
         car1 = createSprite(100,200);
+        car1.addImage(car1Img);
         car2 = createSprite(300,200);
+        car2.addImage(car2Img);
         car3 = createSprite(500,200);
+        car3.addImage(car3Img);
         car4 = createSprite(700,200);
+        car4.addImage(car4Img);
         cars = [car1,car2,car3,car4];
     }
     play(){
@@ -34,13 +38,15 @@ class Game {
        // text("Game Started!",200,100);
         Player.fetchPlayerInfo();
         if(allPlayers !== undefined){
+            background(groundImg);
+            image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5);
             var y;
             var x = 180;
             var index = 0;
             for(var i in allPlayers){
                 //text(allPlayers[i].name+ " : " +allPlayers[i].distance,100,y=y+50);
                 index+=1;
-                x+=200;
+                x+=250;
                 y = displayHeight - allPlayers[i].distance;
                 cars[index-1].x = x;
                 cars[index-1].y = y;
@@ -48,13 +54,20 @@ class Game {
                     cars[index-1].shapeColor = "red";
                     camera.position.x = displayWidth/2;
                     camera.position.y = y;
-                }
+                }  
             }
         }
+        if(player.distance > 5000){
+            gameState = 2;
+        }
+
         if(keyIsDown(UP_ARROW)&&player.index!==null){
             player.distance+=50;
             player.update();
         }
         drawSprites();
+    }
+    end(){
+        console.log("gameOver");
     }
 }
